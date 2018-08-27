26af007f56a090332f230c6899d9eb5c2b50869e
David Hong <davydhong@gmail.com>
2018-08-27 15:11:34 -0700

    finished bare min

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,6 +1,11 @@
 var addEvents = function() {
 	var playerOneTurn = true;
+	var boxArr = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
 	var boxes = document.getElementsByClassName('board');
+	var winCounter = {
+		X: 0,
+		O: 0,
+	};
 
 	var turnSwitch = function() {
 		playerOneTurn = !playerOneTurn;
@@ -12,25 +17,69 @@ var addEvents = function() {
 		}
 	};
 	for (let i = 0; i < boxes.length; i++) {
-		// the following can invoke only one function and
-		// can't handle multiple functions;
 		boxes[i].addEventListener('click', function() {
 			turnSwitch();
-			// console.log(this);
-			markerOX.call(this, playerOneTurn);
+			markerOX.call(this, playerOneTurn, boxArr);
+			EmptyIfFull(boxes);
+			setTimeout(() => whoIsWinner(boxes), 0);
 		});
-		// boxes[i].onclick = function() {
-		// 	console.log(this);
-		// };
 	}
-};
-var markerOX = function(playerOneTurn) {
-	// console.log(this, box);
-	if (playerOneTurn) {
-		this.innerText = 'X';
-	} else {
-		this.innerText = 'O';
-	}
+	/////////////////HELPER FUNCTIONS//////////////
+	///////////////////////////////////////////////
+	var markerOX = function(turnSwitch) {
+		if (turnSwitch) {
+			boxArr[parseInt(this.attributes.row.value)][parseInt(this.attributes.col.value)] = 1;
+			this.innerText = 'X';
+		} else {
+			boxArr[parseInt(this.attributes.row.value)][parseInt(this.attributes.col.value)] = 2;
+			this.innerText = 'O';
+		}
+	};
+
+	var EmptyIfFull = function(boardAsDOM) {
+		var boxFull = !boxArr.map(boxRow => !boxRow.includes(0)).includes(false);
+		if (boxFull) {
+			// console.log('boxFull!', boxArr);
+			alert('Draw!');
+			boxArr = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
+			for (let i = 0; i < boardAsDOM.length; i++) {
+				boardAsDOM[i].innerText = '';
+			}
+		} else {
+			// console.log('boxNotFull!', boxArr);
+		}
+	};
+
+	var whoIsWinner = function(boardAsDOM) {
+		const rowReducer = (accumulator, currentValue) => {
+			if (accumulator === currentValue && accumulator !== 0) {
+				return accumulator;
+			} else {
+				return false;
+			}
+		};
+		const boardReducer = (accumulator, currentValue) => {
+			return accumulator || currentValue;
+		};
+		var boxMod = [...boxArr, ...transpose(boxArr), ...diagonal(boxArr)];
+		var Winner = boxMod.map(boxRow => boxRow.reduce(rowReducer)).reduce(boardReducer);
+
+		if (Winner === 1) {
+			++winCounter.X;
+			alert('Player X Won!');
+			boxArr = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
+			for (let i = 0; i < boardAsDOM.length; i++) {
+				boardAsDOM[i].innerText = '';
+			}
+		} else if (Winner === 2) {
+			++winCounter.O;
+			alert('Player O Won!');
+			boxArr = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
+			for (let i = 0; i < boardAsDOM.length; i++) {
+				boardAsDOM[i].innerText = '';
+			}
+		}
+	};
 };
 
 // Add an event Handle to the thead to switch between
@@ -47,3 +96,36 @@ var markerOX = function(playerOneTurn) {
 // Need a function to check if there is a winner
 // If there is a winner, table footer should say "Player One is the Winner!"
 // Clear the board when the footer is clicked
+
+var transpose = function(matrix) {
+	var newMatrix = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
+	for (let i = 0; i < newMatrix.length; i++) {
+		for (let j = 0; j < newMatrix[i].length; j++) {
+			newMatrix[i][j] = matrix[j][i];
+		}
+	}
+	return newMatrix;
+};
+
+var diagonal = function(matrix) {
+	var newMatrix = [];
+	var majorDiag = [];
+	for (let i = 0; i < matrix.length; i++) {
+		for (let j = 0; j < matrix[i].length; j++) {
+			if (i === j) {
+				majorDiag.push(matrix[i][j]);
+			}
+		}
+	}
+	newMatrix.push(majorDiag);
+	var minorDiag = [];
+	for (let i = 0; i < matrix.length; i++) {
+		for (let j = 0; j < matrix[i].length; j++) {
+			if (i + j === 2) {
+				minorDiag.push(matrix[i][j]);
+			}
+		}
+	}
+	newMatrix.push(minorDiag);
+	return newMatrix;
+};

1ae9a689c9e69adb0fc93be4099b14cd9720a06d
David Hong <davydhong@gmail.com>
2018-09-02 01:11:01 -0700

    style change

--- challenge_4/client/src/components/col.jsx
+++ challenge_4/client/src/components/col.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
 var Col = ({ x, y, handlePlacerMouseEnter, handlePlacerMouseLeave, handlePlaceDisc, valueFromState }) => (
-	<div className={`col ${valueFromState}`} x={x} y={y} onMouseEnter={handlePlacerMouseEnter} onMouseLeave={handlePlacerMouseLeave} onClick={handlePlaceDisc} />
+  <div className={`col ${valueFromState}`} x={x} y={y} onMouseEnter={handlePlacerMouseEnter} onMouseLeave={handlePlacerMouseLeave} onClick={handlePlaceDisc} />
 );
 export default Col;
